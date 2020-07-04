4294b6e52305e9f0d0658ff1d7a5a63a2b2e5fda
Jacob Cunningham <jcun4128@gmail.com>
2020-07-03 23:40:50 -0500

    init commit
    following through tutorial

---
+++ app/src/main/java/com/example/tutorialwidget/ExampleAppWidgetProvider.java
@@ -0,0 +1,12 @@
+package com.example.tutorialwidget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class ExampleAppWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        
+    }
+}
